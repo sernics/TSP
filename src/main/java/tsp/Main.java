b6f0cac9f7696c3009a0927987a56a8adc31e0ab
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-03-17 18:43:49 +0000

    Benchmark tested and done

--- src/main/java/tsp/Main.java
+++ src/main/java/tsp/Main.java
@@ -2,7 +2,7 @@ package src.main.java.tsp;
 
 public class Main {
   public static void main(String[] args) {
-    Integer numberOfInstances = 20;
+    Integer numberOfInstances = 24;
     Benchmark benchmark = new Benchmark();
     benchmark.run(numberOfInstances);
   }
